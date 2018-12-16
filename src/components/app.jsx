0705b2184a145848b9135c7c83f144d36948d33e
Austin Barr <austinbarr@protonmail.com>
2018-12-15 22:04:22 -0800

    added initial files, addded scss compiling

---
+++ src/components/app.jsx
@@ -0,0 +1,19 @@
+import React from 'react';
+import Test from './test.jsx';
+
+class App extends React.Component {
+
+  constructor(props) {
+    super(props);
+  }
+  render(){
+    return (
+      <Test />
+    );
+    
+  }
+}
+
+
+
+export default App;
